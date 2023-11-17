a3c31ee3842a7dee8fdf23dcaf54e2aa73bfcc0b
Hana-kuragechan <147121796+Hana-kuragechan@users.noreply.github.com>
2023-11-17 12:23:43 +0900

    リモートリポジトリでコンフリクト変更

--- Git_Test_Project.cpp
+++ Git_Test_Project.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello VisualStudio Git!\n";
 
-    std::cout << "Pull Request branch change\n";
+    std::cout << "GitHub Clone changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
